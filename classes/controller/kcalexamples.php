7fb8582b56bf408f7fec1a943ef7f0b204f56414
Alex Gisby <alex.gisby@gmail.com>
2011-02-06 22:01:08 +0000

    Started work on making kCal more friendly and useful.
    - Started work on usage examples.

---
+++ classes/controller/kcalexamples.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Examples for using kCal in your projects.
+ *
+ * @package 	kCal
+ * @category  	Examples
+ * @author 		Alex Gisby
+ */
+
+
+class Controller_Kcalexamples extends Controller_Template_Twig
+{
+	/**
+	 * Basic example
+	 */
+	public function action_index()
+	{
+		
+	}
+}
