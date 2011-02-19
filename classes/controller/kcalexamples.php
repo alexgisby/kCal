419d2726a2260b72743e1c99ede4f6534455f6e1
Alex Gisby <alex.gisby@gmail.com>
2011-02-19 10:43:36 +0000

    Basic example of calendar complete

--- classes/controller/kcalexamples.php
+++ classes/controller/kcalexamples.php
@@ -12,10 +12,12 @@
 class Controller_Kcalexamples extends Controller_Template_Twig
 {
 	/**
-	 * Basic example
+	 * Basic examples
 	 */
 	public function action_index()
 	{
-		
+		// Build using today's date;
+		$basic_cal = new kCal();
+		$this->template->basic_calendar = $basic_cal;
 	}
 }
